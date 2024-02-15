5f06ea6dcde1e51ea11056323eb78963686d24c6
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-02-15 20:57:40 -0300

    ordenando

--- src/FinancesLib/Stocks/Funcs.py
+++ src/FinancesLib/Stocks/Funcs.py
@@ -4,6 +4,34 @@ import numpy as np
 import yfinance as yf
 from sklearn.metrics import mutual_info_score
 
+################################################################################
+
+def get_dataframe_of_stocks(stocks_list,labels_list):
+  init=True;
+  for stock in stocks_list:
+    handler = yf.Ticker(stock+'.SA')
+    #for keys,values in handler.info.items():
+    #  print('['+keys+']',values)
+
+    ddict=dict();
+    ddict['name']=[stock];
+    for key in labels_list:
+      ddict[key]=[''];
+      if key in handler.info.keys():
+        ddict[key]=[handler.info[key]];
+    
+    df = pd.DataFrame(ddict);
+
+    if init==True:
+      DF=df.copy();
+    else:
+      DF=pd.concat([DF,df], ignore_index=True);
+
+    init=False;
+  return DF;
+
+################################################################################
+
 def get_corr_matrix(stock_list,period="24mo",post_name='.SA'):
 
     hist=dict();
@@ -21,13 +49,13 @@ def get_corr_matrix(stock_list,period="24mo",post_name='.SA'):
         C[n1,n2]=np.corrcoef(x1,x2)[0,1];
     return C;
 
+################################################################################
 
 def calc_MI(x, y, bins):
     c_xy = np.histogram2d(x, y, bins)[0]
     
     mi = mutual_info_score(None, None, contingency=c_xy)
     return mi
-
 
 def get_nmi_matrix(stock_list,period="24mo",post_name='.SA',bins=8):
 
@@ -46,6 +74,8 @@ def get_nmi_matrix(stock_list,period="24mo",post_name='.SA',bins=8):
         C[n1,n2]=calc_MI(x1,x2,bins);
         
     return C/np.max(C);
+
+################################################################################
 
 import seaborn as sns
 import matplotlib.pyplot as plt
