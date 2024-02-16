d93522b28ea951e6b354bb67d07d192a5a045a52
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-02-16 01:38:24 -0300

    ordenando

--- src/FinancesLib/Dataset/Load.py
+++ src/FinancesLib/Dataset/Load.py
@@ -12,7 +12,7 @@ def load_cvs_nubank_rv(filename, active_type='Ação'):
   df2 = df2.loc[df2['TIPO DE INVESTIMENTO'] == active_type];
   df2 = df2[["DESCRIÇÃO","TIPO DE INVESTIMENTO","QUANTIDADE","VALOR BRUTO"]]
   
-  df2["VALOR BRUTO"]=df2["VALOR BRUTO"].str.replace('R$', '',regex=True).str.replace('.' , '',regex=True).str.replace(',' , '.',regex=True).astype(float);
+  df2["VALOR BRUTO"]=df2["VALOR BRUTO"].str.replace('R\\$', '',regex=True).str.replace('.' , '',regex=True).str.replace(',' , '.',regex=True).astype(float);
   
   #df2["QUANTIDADE"] = pd.to_numeric(df2["QUANTIDADE"])
 
