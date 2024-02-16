de2f9caa7a01eba793d519fd440c3232967d055f
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-02-16 01:11:06 -0300

    ordenando

--- src/FinancesLib/Dataset/Load.py
+++ src/FinancesLib/Dataset/Load.py
@@ -2,11 +2,16 @@
 import pandas as pd
 
 def load_cvs_nubank_rv(filename, active_type='Ação'):
-  df2 = pd.read_csv(filename, encoding = "latin_1",sep=None,skiprows=1,engine = 'python')
+  df2 = pd.read_csv(filename, 
+                    encoding = "latin_1",
+                    sep=None,
+                    skiprows=1,
+                    engine = 'python',
+                    decimal=",", thousands=".")
   
   df2 = df2.loc[df2['TIPO DE INVESTIMENTO'] == active_type];
   df2 = df2[["DESCRIÇÃO","TIPO DE INVESTIMENTO","QUANTIDADE", "VALOR BRUTO"]]
   
-  df2["QUANTIDADE"] = pd.to_numeric(df2["QUANTIDADE"])
+  #df2["QUANTIDADE"] = pd.to_numeric(df2["QUANTIDADE"])
 
   return df2;
