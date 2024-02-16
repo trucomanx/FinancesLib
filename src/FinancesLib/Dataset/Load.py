88e8209646686c12cee31e2c1b2a5c4571688b1b
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-02-16 15:19:45 -0300

    ordenando

--- src/FinancesLib/Dataset/Load.py
+++ src/FinancesLib/Dataset/Load.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import pandas as pd
 
-def load_cvs_nubank_rv(filename, active_type='Ação'):
+def load_cvs_nubank_rv(filename, active_type_list=['Ação']):
     df2 = pd.read_csv(  filename, 
                         encoding = "latin_1",
                         sep=None,
@@ -11,7 +11,12 @@ def load_cvs_nubank_rv(filename, active_type='Ação'):
                         thousands="."
                         )
     
-    df2 = df2.loc[df2['TIPO DE INVESTIMENTO'] == active_type];
+    for n in range(len(active_type_list)):
+        if n==0:
+            BOOL=df2['TIPO DE INVESTIMENTO'] == active_type_list[0];
+        else:
+            BOOL=BOOL or (df2['TIPO DE INVESTIMENTO'] == active_type_list[n]);
+    df2 = df2.loc[BOOL];
     df2 = df2[["DESCRIÇÃO","TIPO DE INVESTIMENTO","QUANTIDADE","VALOR BRUTO"]]
   
     df2["VALOR BRUTO"]=df2["VALOR BRUTO"].str.replace('R\\$', '',regex=True).str.replace('.' , '',regex=True).str.replace(',' , '.',regex=True).astype(float);
