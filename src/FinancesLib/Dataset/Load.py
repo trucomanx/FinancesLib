af6892c858769eb0c52273e9c65fbb58291833ef
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-02-16 02:22:42 -0300

    ordenando

--- src/FinancesLib/Dataset/Load.py
+++ src/FinancesLib/Dataset/Load.py
@@ -2,18 +2,20 @@
 import pandas as pd
 
 def load_cvs_nubank_rv(filename, active_type='Ação'):
-  df2 = pd.read_csv(filename, 
-                    encoding = "latin_1",
-                    sep=None,
-                    skiprows=1,
-                    engine = 'python',
-                    decimal=",", thousands=".")
+    df2 = pd.read_csv(  filename, 
+                        encoding = "latin_1",
+                        sep=None,
+                        skiprows=1,
+                        engine = 'python',
+                        decimal=",", 
+                        thousands="."
+                        )
+    
+    df2 = df2.loc[df2['TIPO DE INVESTIMENTO'] == active_type];
+    df2 = df2[["DESCRIÇÃO","TIPO DE INVESTIMENTO","QUANTIDADE","VALOR BRUTO"]]
   
-  df2 = df2.loc[df2['TIPO DE INVESTIMENTO'] == active_type];
-  df2 = df2[["DESCRIÇÃO","TIPO DE INVESTIMENTO","QUANTIDADE","VALOR BRUTO"]]
-  
-  df2["VALOR BRUTO"]=df2["VALOR BRUTO"].str.replace('R\\$', '',regex=True).str.replace('.' , '',regex=True).str.replace(',' , '.',regex=True).astype(float);
+    df2["VALOR BRUTO"]=df2["VALOR BRUTO"].str.replace('R\\$', '',regex=True).str.replace('.' , '',regex=True).str.replace(',' , '.',regex=True).astype(float);
   
   #df2["QUANTIDADE"] = pd.to_numeric(df2["QUANTIDADE"])
 
-  return df2;
+    return df2;
--- src/FinancesLib/Stocks/Funcs.py
+++ src/FinancesLib/Stocks/Funcs.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 import yfinance as yf
-from sklearn.metrics import mutual_info_score
+
 
 ################################################################################
 
@@ -30,65 +30,4 @@ def get_dataframe_of_stocks(stocks_list,labels_list):
     init=False;
   return DF;
 
-################################################################################
 
-def get_corr_matrix(stock_list,period="24mo",post_name='.SA'):
-
-    hist=dict();
-    for stock in stock_list:
-      handler = yf.Ticker(stock.upper()+post_name.upper())
-      hist[stock]=handler.history(period=period);
-      
-    C=np.zeros((len(stock_list),len(stock_list)));
-    
-    for n1 in range(len(stock_list)):
-      for n2 in range(len(stock_list)):
-        x1=hist[stock_list[n1]]['Open'].to_numpy();
-        x2=hist[stock_list[n2]]['Open'].to_numpy();
-
-        C[n1,n2]=np.corrcoef(x1,x2)[0,1];
-    return C;
-
-################################################################################
-
-def calc_MI(x, y, bins):
-    c_xy = np.histogram2d(x, y, bins)[0]
-    
-    mi = mutual_info_score(None, None, contingency=c_xy)
-    return mi
-
-def get_nmi_matrix(stock_list,period="24mo",post_name='.SA',bins=8):
-
-    hist=dict();
-    for stock in stock_list:
-      handler = yf.Ticker(stock.upper()+post_name.upper())
-      hist[stock]=handler.history(period=period);
-      
-    C=np.zeros((len(stock_list),len(stock_list)));
-    
-    for n1 in range(len(stock_list)):
-      for n2 in range(len(stock_list)):
-        x1=hist[stock_list[n1]]['Open'].to_numpy();
-        x2=hist[stock_list[n2]]['Open'].to_numpy();
-
-        C[n1,n2]=calc_MI(x1,x2,bins);
-        
-    return C/np.max(C);
-
-################################################################################
-
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-def plot_stocks_heatmap(mat,stocks,cmap='gray',fontsize=13,title='',figsize=(14,14)):
-    plt.figure(figsize=figsize)
-    sns.heatmap(mat, 
-                annot=True,
-                fmt='g', 
-                cmap=cmap,#"jet",
-                xticklabels=stocks,
-                yticklabels=stocks)
-    plt.ylabel('stocks',fontsize=fontsize)
-    plt.xlabel('stocks',fontsize=fontsize)
-    plt.title(title,fontsize=fontsize)
-    plt.show()
