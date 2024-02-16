34cd865902009fc6e163fd8923f14afb8ea29060
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-02-16 01:16:44 -0300

    ordenando

--- src/FinancesLib/Dataset/Load.py
+++ src/FinancesLib/Dataset/Load.py
@@ -10,7 +10,11 @@ def load_cvs_nubank_rv(filename, active_type='Ação'):
                     decimal=",", thousands=".")
   
   df2 = df2.loc[df2['TIPO DE INVESTIMENTO'] == active_type];
-  df2 = df2[["DESCRIÇÃO","TIPO DE INVESTIMENTO","QUANTIDADE", "VALOR BRUTO"]]
+  df2 = df2[["DESCRIÇÃO","TIPO DE INVESTIMENTO","QUANTIDADE","VALOR BRUTO"]]
+  
+  df2["VALOR BRUTO"].str.replace('R$', '');
+  df2["VALOR BRUTO"].str.replace('.' , '');
+  df2["VALOR BRUTO"].str.replace(',' , '.');
   
   #df2["QUANTIDADE"] = pd.to_numeric(df2["QUANTIDADE"])
 
