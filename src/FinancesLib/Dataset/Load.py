465e65adcb22642cb84a656770e5f7344da23d29
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-02-16 17:44:35 -0300

    ordenando

--- src/FinancesLib/Dataset/Load.py
+++ src/FinancesLib/Dataset/Load.py
@@ -1,6 +1,27 @@
 #!/usr/bin/python
 import pandas as pd
 
+def load_cvs_nuinvest_all(filename):
+    '''
+    Carrega datos no forato do CSV de NUINVEST
+    '''
+    df2 = pd.read_csv(  filename, 
+                        encoding = "latin_1",
+                        sep=None,
+                        skiprows=1,
+                        engine = 'python',
+                        decimal=",", 
+                        thousands="."
+                        )
+    df2["VALOR APLICADO"]=df2["VALOR APLICADO"].str.replace('R\\$', '',regex=True).str.replace('.' , '',regex=True).str.replace(',' , '.',regex=True).astype(float);
+    df2["VALOR BRUTO"]=df2["VALOR BRUTO"].str.replace('R\\$', '',regex=True).str.replace('.' , '',regex=True).str.replace(',' , '.',regex=True).astype(float);
+    df2["VALOR LÍQUIDO"]=df2["VALOR LÍQUIDO"].str.replace('R\\$', '',regex=True).str.replace('.' , '',regex=True).str.replace(',' , '.',regex=True).astype(float);
+    df2["RENDIMENTO R$"]=df2["RENDIMENTO R$"].str.replace('R\\$', '',regex=True).str.replace('.' , '',regex=True).str.replace(',' , '.',regex=True).astype(float);
+    df2["RENDIMENTO %"]=df2["RENDIMENTO %"].str.replace('\\%', '',regex=True).str.replace('.' , '',regex=True).str.replace(',' , '.',regex=True).astype(float);
+
+    return df2;
+
+
 def load_cvs_nuinvest_rv(filename, active_type_list=['Ação']):
     '''
     Carrega datos no forato do CSV de NUINVEST
