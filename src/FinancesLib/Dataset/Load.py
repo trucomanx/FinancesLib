30299377432a4f5388fe92efa9c7f6aab72ac768
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-02-16 01:03:21 -0300

    ordenando

--- src/FinancesLib/Dataset/Load.py
+++ src/FinancesLib/Dataset/Load.py
@@ -6,5 +6,7 @@ def load_cvs_nubank_rv(filename, active_type='Ação'):
   
   df2 = df2.loc[df2['TIPO DE INVESTIMENTO'] == active_type];
   df2 = df2[["DESCRIÇÃO","TIPO DE INVESTIMENTO","QUANTIDADE", "VALOR BRUTO"]]
+  
+  df2["QUANTIDADE"] = pd.to_numeric(df2["QUANTIDADE"])
 
   return df2;
