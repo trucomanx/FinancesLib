a0c37f399ab4d37dc53b9ed030f32888067d79b1
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-02-17 10:49:19 -0300

    ordenando

--- src/FinancesLib/Stocks/Analysis.py
+++ src/FinancesLib/Stocks/Analysis.py
@@ -7,31 +7,34 @@ from sklearn.metrics import mutual_info_score
 
 ################################################################################
 
-def get_corr_matrix(stock_list,period="24mo",post_name=''):
+def get_corr_matrix(stock_list,period="24mo",post_name='',verbose=True):
     '''
     Retorna la matriz de correlacion de los precios de cada un de las acciones de 
     una lista de acciones stock_list.
     '''
     hist=dict();
     for stock in stock_list:
-      handler = yf.Ticker(stock.upper()+post_name.upper())
-      hist[stock]=handler.history(period=period);
+        handler = yf.Ticker(stock.upper()+post_name.upper())
+        hist[stock]=handler.history(period=period);
       
     C=np.zeros((len(stock_list),len(stock_list)));
     
     for n1 in range(len(stock_list)):
-      for n2 in range(len(stock_list)):
-        x1=hist[stock_list[n1]]['Open'].to_numpy();
-        x2=hist[stock_list[n2]]['Open'].to_numpy();
-        
-        if (x1.size>0) and (x2.size>0):
-            L=x1.size;
-            if x1.size!=x2.size:
-                L=min(x1.size,x2.size);
-                print(stock_list[n1],x1.size,stock_list[n2],x2.size)
-            C[n1,n2]=np.corrcoef(x1[0:L],x2[0:L])[0,1];
-        else:
-            C[n1,n2]=0.0;
+        for n2 in range(len(stock_list)):
+            x1=hist[stock_list[n1]]['Open'].to_numpy();
+            x2=hist[stock_list[n2]]['Open'].to_numpy();
+            
+            if (x1.size>0) and (x2.size>0):
+                L=x1.size;
+                if x1.size!=x2.size:
+                    L=min(x1.size,x2.size);
+                    if verbose:
+                        print(stock_list[n1],x1.size,stock_list[n2],x2.size)
+                C[n1,n2]=np.corrcoef(x1[0:L],x2[0:L])[0,1];
+            else:
+                if verbose:
+                    print(stock_list[n1],x1.size,stock_list[n2],x2.size)
+                C[n1,n2]=0.0;
     return C;
 
 ################################################################################
@@ -42,7 +45,7 @@ def calc_MI(x, y, nbins):
     mi = mutual_info_score(None, None, contingency=c_xy)
     return mi
 
-def get_nmi_matrix(stock_list,period="24mo",post_name='',nbins=8):
+def get_nmi_matrix(stock_list,period="24mo",post_name='',nbins=8,verbose=True):
     '''
     Retorna la matriz de informacion mutua de los precios de cada un de las acciones de 
     una lista de acciones stock_list.
@@ -50,25 +53,27 @@ def get_nmi_matrix(stock_list,period="24mo",post_name='',nbins=8):
     '''
     hist=dict();
     for stock in stock_list:
-      handler = yf.Ticker(stock.upper()+post_name.upper())
-      hist[stock]=handler.history(period=period);
+        handler = yf.Ticker(stock.upper()+post_name.upper())
+        hist[stock]=handler.history(period=period);
       
     C=np.zeros((len(stock_list),len(stock_list)));
     
     for n1 in range(len(stock_list)):
-      for n2 in range(len(stock_list)):
-        x1=hist[stock_list[n1]]['Open'].to_numpy();
-        x2=hist[stock_list[n2]]['Open'].to_numpy();
+        for n2 in range(len(stock_list)):
+            x1=hist[stock_list[n1]]['Open'].to_numpy();
+            x2=hist[stock_list[n2]]['Open'].to_numpy();
         
-        if (x1.size>0) and (x2.size>0):
-            L=x1.size;
-            if x1.size!=x2.size:
-                L=min(x1.size,x2.size);
-                print(stock_list[n1],x1.size,stock_list[n2],x2.size)
-            C[n1,n2]=calc_MI(x1[0:L],x2[0:L],nbins);
-        else:
-            print(stock_list[n1],x1.size,stock_list[n2],x2.size)
-            C[n1,n2]=0.0;
+            if (x1.size>0) and (x2.size>0):
+                L=x1.size;
+                if x1.size!=x2.size:
+                    L=min(x1.size,x2.size);
+                    if verbose:
+                        print(stock_list[n1],x1.size,stock_list[n2],x2.size)
+                C[n1,n2]=calc_MI(x1[0:L],x2[0:L],nbins);
+            else:
+                if verbose:
+                    print(stock_list[n1],x1.size,stock_list[n2],x2.size)
+                C[n1,n2]=0.0;
         
     return C/np.max(C);
 
